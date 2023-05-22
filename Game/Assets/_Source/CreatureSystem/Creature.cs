03befff3e3184922585f046623b10fb4bc226103
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-05-22 13:03:41 +0300

    Creature vision system

--- Game/Assets/_Source/CreatureSystem/Creature.cs
+++ Game/Assets/_Source/CreatureSystem/Creature.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = System.Random;
@@ -10,10 +8,9 @@ namespace CreatureSystem
     public class Creature : MonoBehaviour
     {
         [SerializeField] private Transform head;
-        [SerializeField] private Transform search;
-        [SerializeField] private Transform test1;
-        [SerializeField] private Transform test2;
+        [SerializeField] private float distance;
         [SerializeField] private Transform[] point;
+        [SerializeField] private float fovAngel;
         [SerializeField] private NavMeshAgent navMeshAgent;
         [SerializeField] private LayerMask player;
 
@@ -30,17 +27,18 @@ namespace CreatureSystem
         {
             if (PlayerFind())
             {
-                StopAllCoroutines();
+                // StopAllCoroutines();
+                StopCoroutine(Inspection());
+                
             }
             else if (navMeshAgent.enabled
                 && navMeshAgent.remainingDistance == 0)
             {
-                StartCoroutine(Walk());
-                // yield return new WaitForSeconds(3f);
+                StartCoroutine(Inspection());
             }
         }
 
-        private IEnumerator Walk()
+        private IEnumerator Inspection()
         {
             navMeshAgent.enabled = false;
             var rotationHead = 0;
@@ -66,12 +64,21 @@ namespace CreatureSystem
 
         private bool PlayerFind()
         {
-            // Physics.Raycast(transform.position, transform.forward, Mathf.Infinity, player);
-            RaycastHit hit;
-            if (Physics.SphereCast(head.position, 50f, Vector3.zero, out hit, player))
+            Collider[] col = Physics.OverlapSphere(head.position, distance, player);
+            if (col.Length != 0)
             {
-                Physics.Raycast(head.position, hit.transform.position);
+                Vector3 dir = col[0].transform.position - head.position;
+                float angle = Vector3.Angle(dir, head.forward);
+                RaycastHit hit;
+                if (Physics.Raycast(head.position, dir, out hit, distance, player)
+                    && angle < fovAngel / 2)
+                {
+                    Debug.DrawRay(head.position, dir, Color.red);
+                    navMeshAgent.SetDestination(col[0].transform.position);
+                    return true;
+                }
             }
+
             return false;
         }
 
@@ -83,30 +90,27 @@ namespace CreatureSystem
 
         private void OnDrawGizmos()
         {
-            RaycastHit hit;
-            // Physics.Raycast(transform.position, transform.forward, out hit,Mathf.Infinity, player);
-            Gizmos.color = Color.white;
-            Gizmos.DrawWireSphere(head.position, 5f);
-            Gizmos.color = Color.red;
-            // if (Physics.SphereCast(head.position, 5f, head.forward, out hit))
+            // RaycastHit hit;
+            // // Physics.Raycast(transform.position, transform.forward, out hit,Mathf.Infinity, player);
+            // Gizmos.color = Color.white;
+            // Gizmos.DrawWireSphere(head.position, 5f);
+            // Gizmos.color = Color.red;
+            // // if (Physics.SphereCast(head.position, 5f, head.forward, out hit))
+            // // {
+            // //     Gizmos.DrawLine(head.position, hit.transform.position);
+            // // }
+            // Collider[] col = Physics.OverlapSphere(head.position, 5f, player);
+            // if (col.Length != 0
+            //     && col[0].transform)
             // {
-            //     Gizmos.DrawLine(head.position, hit.transform.position);
+            //     // Physics.Raycast(head.position, col[0].transform.position, out hit, Mathf.Infinity, player);
+            //     // Gizmos.DrawLine(head.position, col[0].transform.position);
+            //     search.LookAt(col[0].transform);
+            //     if (search.rotation.eulerAngles.y is > -45 and < 45)
+            //     {
+            //         Debug.Log("I see u");
+            //     }
             // }
-            Collider[] col = Physics.OverlapSphere(head.position, 5f, player);
-            if (col.Length != 0
-                && col[0].transform)
-            {
-                // Physics.Raycast(head.position, col[0].transform.position, out hit, Mathf.Infinity, player);
-                // Gizmos.DrawLine(head.position, col[0].transform.position);
-                search.LookAt(col[0].transform);
-                if (search.rotation.eulerAngles.y is > -45 and < 45)
-                {
-                    Debug.Log("I see u");
-                }
-            }
-
-            Gizmos.DrawLine(test1.position, test1.forward * 10f);
-            Gizmos.DrawLine(test2.position, test2.forward * 10f);
         }
     }
 }
